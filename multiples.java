8da18fcea4d5076a4686925d4a126b59db6b2acb
Jason <32491211+j-kingston@users.noreply.github.com>
2017-12-05 21:19:58 -0600

    Add files via upload

---
+++ multiples.java
@@ -0,0 +1,26 @@
+public class multiples {
+	
+	public static void main(String args[]){
+		int x = 0, sum = 0;
+		while(x < 1000) {
+			if(x % 15 == 0) {
+				sum += x;
+				x++;
+			}
+			else if(x % 3 == 0) {
+				sum += x;
+				x++;
+			}
+			else if(x % 5 == 0) {
+				sum += x;
+				x++;
+			}
+			else {
+				x++;
+			}
+		}
+		System.out.print(sum);
+	}
+}
+	
+
