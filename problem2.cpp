fd24815c34383da1a9d60508602e40e3fff7abbd
Jason <32491211+j-kingston@users.noreply.github.com>
2017-12-06 14:27:29 -0600

    Add files via upload

---
+++ problem2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int sum = 0;
+
+int sumOfSquares(int n) {
+  while (n <= 100) {
+    int N = n * n;
+    sum += N;
+    if (n == 100) {
+      return sum;
+    }
+    n++;
+    return sumOfSquares(n);
+  }
+ 
+}
+
+int squareOfSums(int m) {
+  if (m < 100) {
+    sum += m;
+    m++;
+    return squareOfSums(m);
+  }
+  if (m == 100) {
+    sum += m;
+    return sum * sum;
+  }
+
+}
+
+int main() {
+  int i = 1;
+  sumOfSquares(i);
+  cout << "Sum of squares is: " << sum << endl;
+  sum = 0;
+  cout << "Square of sums is: " << squareOfSums(i) << endl;
+  cin.get();
+  return 0;
+}
